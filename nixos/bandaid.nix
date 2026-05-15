bd736a1f69b38ed32f13044944d386c4e6ed3beb
Mio <mio@mio19.uk>
2026-05-15 20:32:02 +1000

    fix

--- nixos/bandaid.nix
+++ nixos/bandaid.nix
@@ -6,10 +6,11 @@
   system,
   ...
 }@args:
-assert builtins.elem "yama" config.security.lsm;
 {
   # https://git.kernel.org/torvalds/c/31e62c2ebbfd
-  boot.kernel.sysctl."kernel.yama.ptrace_scope" = 2;
+  boot.kernel.sysctl."kernel.yama.ptrace_scope" =
+    assert builtins.elem "yama" config.security.lsm;
+    2;
 
   # Dirty Frag and https://github.com/v12-security/pocs/tree/main/fragnesia https://lists.openwall.net/netdev/2026/05/13/79
   boot.blacklistedKernelModules = [
