2b869a67c57fdfd1f5f77cc4bb9c3471eb004144
Mio <mio@mio19.uk>
2026-06-10 00:19:07 +1000

    speed = 36;

--- nixos/fw13/fw13.nix
+++ nixos/fw13/fw13.nix
@@ -25,7 +25,7 @@ with _include;
       strategyOnDischarging = "dischargingstrategy";
       # https://community.frame.work/t/amd-7840u-fan-issues/69704/2
       # https://community.frame.work/t/fan-hysterersis-issue/4469/4
-      # avoid clicking sound : jump from 0 to speed = 15; directly to speed = 38;
+      # avoid clicking sound : jump from 0 to speed = 15; directly to speed = 36;
       strategies = {
         "dischargingstrategy" = {
           fanSpeedUpdateFrequency = 7;
@@ -49,11 +49,11 @@ with _include;
             }
             {
               temp = 48;
-              speed = 38;
+              speed = 36;
             }
             {
               temp = 70;
-              speed = 38;
+              speed = 36;
             }
             {
               temp = 80;
@@ -87,11 +87,11 @@ with _include;
             }
             {
               temp = 45;
-              speed = 38;
+              speed = 36;
             }
             {
               temp = 70;
-              speed = 38;
+              speed = 36;
             }
             {
               temp = 80;
