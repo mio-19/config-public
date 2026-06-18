098d16743f0367e209d1869f1a4f167bb660b055
Mio <mio@mio19.uk>
2026-06-18 14:31:39 +1000

    act_pedit

--- nixos/bandaid/packet_edit_meme.nix
+++ nixos/bandaid/packet_edit_meme.nix
@@ -4,12 +4,8 @@
   # Linux Kernel net/sched act_pedit LPE
   # https://git.kernel.org/linus/899ee91156e57784090c5565e4f31bd7dbffbc5a
   # https://github.com/sgkdev/packet_edit_meme
-  # unsure how to mitigate this correctly
-  security.unprivilegedUsernsClone = false;
-  security.apparmor.killUnconfinedConfinables = true;
-  boot.blacklistedKernelModules = [
-    "cls_basic"
-    "cls_matchall"
-    "em_meta"
-  ];
+  boot.blacklistedKernelModules = [ "act_pedit" ];
+  boot.extraModprobeConfig = ''
+    install act_pedit /run/current-system/sw/bin/false
+  '';
 }
