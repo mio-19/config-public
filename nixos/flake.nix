d25cfcfe54045c189011ad3c90fd5e3d7a7f4406
Mio <mio@mio19.uk>
2026-06-20 01:54:39 +1000

    idk

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -8,6 +8,7 @@
     nixpkgs-2511.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-2505.url = "github:NixOS/nixpkgs/release-25.05";
     #nixpkgs-small.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    # IS IT NECESSARY TO DO SO MUCH JUST TO GET KDE PLASMA EARLY
     nixpkgs-small.url = "github:NixOS/nixpkgs/d4fea6b6bfce7b55c6df36fb973205b89d7fe761"; # a commit from nixos-unstable-small
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs.follows = "nixpkgs-small";
