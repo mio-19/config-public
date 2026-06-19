70e5f7129fc784e07e3da182c15c820551c8c14b
Mio <mio@mio19.uk>
2026-06-20 01:43:54 +1000

    nixtamal

--- nixos/common.nix
+++ nixos/common.nix
@@ -114,7 +114,7 @@ with _include;
       inherit (pkgs-pin2) ryubing gnome-disk-utility;
       inherit (pkgs-nocuda) freecad; # no binary cache
       inherit (pkgs-unstable) chromium; # no binary cache
-      inherit (pkgs-pin3) pianotrans;
+      inherit (pkgs-pin3) pianotrans nixtamal;
     })
     inputs.chaotic.overlays.default
     inputs.mac-style-plymouth.overlays.default
