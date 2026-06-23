a2e7fc6224dc11651056a8768d7eb1b0e1ab60ac
Mio <mio@mio19.uk>
2026-06-24 10:54:54 +1200

    save

--- mac/include.nix
+++ mac/include.nix
@@ -145,7 +145,7 @@ upper
       cd ~/Documents/config/mac
       ${lib.getExe program.git} config pull.rebase false
       sudo true
-      ${lib.getExe program.git}  -c http.lowSpeedLimit=10000 -c http.lowSpeedTime=10 -c core.sshCommand="ssh -o ConnectTimeout=15" pull || true
+      ${lib.getExe program.git} -c http.lowSpeedLimit=10000 -c http.lowSpeedTime=10 -c core.sshCommand="ssh -o ConnectTimeout=15" pull || true
       sudo nice -n 20 darwin-rebuild switch --flake ~/Documents/config/mac --print-build-logs "$@" |& ${lib.getExe pkgs.nix-output-monitor}
     '';
   };
