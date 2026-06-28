85350db8831069bd8d1638e68b1932fbfb7f044c
Mio <mio@mio19.uk>
2026-06-28 22:18:54 +1200

    vscode-extensions.haskell.haskell to avoid hash issue?

--- home-common.nix
+++ home-common.nix
@@ -14,7 +14,7 @@ let
       # also available from nixpkgs vscode-extensions. :
       #neveruse#nix-vscode-extensions.vscode-marketplace.scalameta.metals
       nix-vscode-extensions.vscode-marketplace.scala-lang.scala
-      nix-vscode-extensions.vscode-marketplace.haskell.haskell
+      vscode-extensions.haskell.haskell
       nix-vscode-extensions.vscode-marketplace.justusadam.language-haskell # haskell.haskell really depends on this??
       nix-vscode-extensions.vscode-marketplace.tomoki1207.pdf
       #nix-vscode-extensions.vscode-marketplace.bbenoist.nix
