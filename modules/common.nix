ac975644e47d5bb3260d44792084a9d95eff0d1b
Mio <mio@mio19.uk>
2026-07-04 17:38:04 +1200

    Revert "common: set SEM_NO_TELEMETRY=1 to disable sem telemetry."
    This reverts commit de377e7cbcb5c8f488530ebfe427c478194febcc.

--- modules/common.nix
+++ modules/common.nix
@@ -25,8 +25,6 @@
         ...
       }:
       {
-        environment.sessionVariables.SEM_NO_TELEMETRY = "1"; # sem: disable anonymous usage telemetry
-
         nix = {
           settings = {
             lint-url-literals = "fatal";
