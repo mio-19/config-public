d197afbce445f4de79ad435e7ed657d40e439fa0
Mio <mio@mio19.uk>
2026-07-05 14:31:02 +1200

    nix.settings.allowed-users = [ "@staff" "@daemon" ]; # break. is it becaused of auto-allocated-uids? or?

--- modules/common.nix
+++ modules/common.nix
@@ -589,7 +589,7 @@
           ../users.nix
         ];
 
-        nix.settings.allowed-users = [ "@staff" "@wheel" "@daemon" ];
+        #nix.settings.allowed-users = [ "@staff" "@daemon" ]; # break. is it becaused of auto-allocated-uids? or?
 
         services.mac-app-util.enable = mac-app-util-enabled;
 
