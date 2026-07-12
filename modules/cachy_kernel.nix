3ed449f8211358faea2d8a2a6e9f2e11931ec811
Mio <mio@mio19.uk>
2026-07-12 15:28:19 +1200

    pkgs-chaotic.linuxPackages_cachyos

--- modules/cachy_kernel.nix
+++ modules/cachy_kernel.nix
@@ -17,17 +17,14 @@
         cachyKernel =
           if config.virtualisation.vmware.host.enable then
             pkgs-chaotic.linuxPackages_cachyos-gcc
-          else if
-            config.microarch == "zen4" && !config.workaround_i_dont_know_kernel_nvidia_refer_problem
-          then
-            /*
-              pkgs-chaotic.linuxPackages_cachyos.cachyOverride {
-                cachyVars = pkgs.linuxPackages_cachyos.kernel.cachyConfig.cachyVars // {
-                  "_processor_opt" = "ZEN4";
-                };
-              }
-            */
-            pkgs-chaotic.linuxPackages_cachyos-lto-znver4
+          else if config.microarch == "zen4" then
+            (
+              if config.workaround_i_dont_know_kernel_nvidia_refer_problem then
+                pkgs-chaotic.linuxPackages_cachyos
+              else
+                pkgs-chaotic.linuxPackages_cachyos-lto-znver4
+            )
+              pkgs-chaotic.linuxPackages_cachyos-lto-znver4
           else
             pkgs-chaotic.linuxPackages_cachyos;
       in
