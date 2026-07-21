b52dce733d7a098646be41b9c1cd7365c497fae7
Mio <mio@mio19.uk>
2026-07-21 12:42:17 +1200

    inherit (pkgs-pin6) nixtamal; # build failed

--- modules/common.nix
+++ modules/common.nix
@@ -643,6 +643,7 @@
               ;
             inherit (pkgs-pin3) zed-editor; # no binary cache
             inherit (pkgs-pin7) vscode; # build failed
+            inherit (pkgs-pin6) nixtamal; # build failed
           })
         ];
         home-manager.sharedModules = [
