5c3cb3afa01b7cb581cc41bab911278be9ace2a6
Mio <mio@mio19.uk>
2026-07-26 13:49:46 +1000

    docs: explicitly document a1ive patch requirement

--- modules/grub-pointer.nix
+++ modules/grub-pointer.nix
@@ -14,6 +14,8 @@
           # Note: The system must boot in UEFI mode so GRUB can use EFI pointer protocols.
           # A graphical theme utilizing gfxmenu is required.
           # Make sure to set gfxmodeEfi to your native resolution to ensure proper touch coordinate mapping.
+          # WARNING: This requires the a1ive GRUB2 patches (`mio.grub2_patched`) to work, 
+          # as standard GRUB does not support `efi_mouse` or `terminal_input mouse`.
 
           extraConfig = ''
             insmod gfxterm
