bcada7aa4a97c8f45ab60f7797f95faedb89d68c
Mio <mio@mio19.uk>
2026-07-29 21:00:54 +1200

    plasma-login-manager_instead = false; # plm seems to take more seconds to launch?

--- modules/hosts/fw13/_nixos/default.nix
+++ modules/hosts/fw13/_nixos/default.nix
@@ -34,7 +34,7 @@ with _include;
   security.pam.zfs.homes = "${pool}/nixos/safe/encrypted";
   chaotic.zfs-impermanence-on-shutdown.volume = "${pool}/nixos/local/ephemeral";
 
-  plasma-login-manager_instead = true;
+  plasma-login-manager_instead = false; # plm seems to take more seconds to launch?
 
   security.allowSimultaneousMultithreading = true;
 
