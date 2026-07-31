bfaf583b9188b8405637af8d33654af2ca1eefd7
Mio <mio@mio19.uk>
2026-07-31 18:47:22 +1200

    fixup hopefully

--- modules/desktop-full.nix
+++ modules/desktop-full.nix
@@ -523,7 +523,7 @@ in
           ++ apps.clean
           ++ apps.cleanX86
           ++ [
-            pkgs-pin3.trayscale
+            trayscale
           ];
         homebrew.casks = [
           "zotero" # version from nixpkgs does not work
