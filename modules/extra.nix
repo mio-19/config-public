8f4d9a725b4a1e92990e719d61893f19eebfd523
Mio <mio@mio19.uk>
2026-08-07 22:02:42 +1200

    inputs.mio.packages.${pkgs.stdenv.hostPlatform.system}.exo

--- modules/extra.nix
+++ modules/extra.nix
@@ -207,6 +207,7 @@ let
           yt-dlp
           #easyeda2kicad
           #interactive-html-bom
+          inputs.mio.packages.${pkgs.stdenv.hostPlatform.system}.exo
           # unfree:
           cursor-cli
 
