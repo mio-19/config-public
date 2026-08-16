f9a01b1c72059fc8b1112a731bcf74b57bfe4c0f
Mio <mio@mio19.uk>
2026-08-16 12:34:25 +1000

    fixup

--- mac/flake.nix
+++ mac/flake.nix
@@ -173,10 +173,6 @@
                   hash = "sha256-sPUmyXp5ENX/0Q5TMy+gEEBjwXtSQHxpqjqCYGCBG2E=";
                 })
                 (fetchpatch {
-                  url = "https://github.com/NixOS/nixpkgs/commit/b42f6f7412e3.diff";
-                  hash = "sha256-ZrfiyNZwpy2waATmgBmwwMJ5w6S3JEK3ydmmNLwVyjs=";
-                })
-                (fetchpatch {
                   name = "types.path.check: Avoid derivation instantiation";
                   url = "https://github.com/NixOS/nixpkgs/pull/540399.patch";
                   hash = "sha256-rJ+c2Wvwt5fr1c4HdQR8QAyhdfXfrDtiAnONnNgEuIo=";
