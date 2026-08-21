87d9961e1b88a9b899567f7dd68568cb74ff6bd2
Mio <mio@mio19.uk>
2026-08-21 22:54:46 +1000

    workaround_i_dont_know_kernel_nvidia_refer_problem

--- modules/cachy_kernel.nix
+++ modules/cachy_kernel.nix
@@ -13,36 +13,52 @@
         _include = args._include or (import ../nixos/include.nix args);
       in
       with _include;
-      let
-        cachyKernel =
-          if
-            config.virtualisation.vmware.host.enable
-            || builtins.hasAttr "bcachefs" config.boot.supportedFilesystems
-          then
-            pkgs-chaotic.linuxPackages_cachyos-gcc
-          else if config.microarch == "zen4" then
-            (
-              if config.workaround_i_dont_know_kernel_nvidia_refer_problem then
-                pkgs-chaotic.linuxPackages_cachyos
-              else if config.workaround_i_dont_know_nvidia_refer_problem_workaround_b then
-                pkgs.linuxPackages_cachyos-lto-znver4
-              else
-                pkgs-chaotic.linuxPackages_cachyos-lto-znver4
-            )
-          else
-            pkgs-chaotic.linuxPackages_cachyos;
-      in
       {
         options = {
           workaround_i_dont_know_kernel_nvidia_refer_problem = lib.mkOption {
-            type = lib.types.bool;
+            type = lib.types.enum [
+              false
+              "no-zen4"
+              "pkgs.zen4"
+              "pkgs.no-zen4"
+              "nixpkgs"
+            ];
             default = false;
             description = "I don't know why it does not or does work";
           };
         };
         config = {
-          boot.kernelPackages = cachyKernel;
-          boot.zfs.package = pkgs.zfs_cachyos;
+          boot.kernelPackages =
+            if
+              config.virtualisation.vmware.host.enable
+              || builtins.hasAttr "bcachefs" config.boot.supportedFilesystems
+            then
+              pkgs-chaotic.linuxPackages_cachyos-gcc
+            else if config.microarch == "zen4" then
+              (
+                if config.workaround_i_dont_know_kernel_nvidia_refer_problem == "no-zen4" then
+                  pkgs-chaotic.linuxPackages_cachyos
+                else if config.workaround_i_dont_know_kernel_nvidia_refer_problem == "pkgs.zen4" then
+                  pkgs.linuxPackages_cachyos-lto-znver4
+                else if config.workaround_i_dont_know_kernel_nvidia_refer_problem == "pkgs.no-zen4" then
+                  pkgs.linuxPackages_cachyos
+                else if config.workaround_i_dont_know_kernel_nvidia_refer_problem == "nixpkgs" then
+                  pkgs.linuxPackages_latest
+                else
+                  assert config.workaround_i_dont_know_kernel_nvidia_refer_problem == false;
+                  pkgs-chaotic.linuxPackages_cachyos-lto-znver4
+              )
+            else
+              pkgs-chaotic.linuxPackages_cachyos;
+          boot.zfs.package =
+            if
+              (
+                config.microarch == "zen4" && config.workaround_i_dont_know_kernel_nvidia_refer_problem == "nixpkgs"
+              )
+            then
+              pkgs.zfs
+            else
+              pkgs.zfs_cachyos;
         };
       };
   };
