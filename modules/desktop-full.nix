d894a15db3a77b6432976141dce34aa35bb96a29
Mio <mio@mio19.uk>
2026-08-23 04:19:19 +1000

    libreoffice-qt-stable

--- modules/desktop-full.nix
+++ modules/desktop-full.nix
@@ -286,7 +286,7 @@
             fluffychat
             progs.zulip
             normcap
-            (if qtIsPreferred then libreoffice-qt6-fresh else libreoffice-fresh)
+            (if qtIsPreferred then libreoffice-qt-stable else libreoffice-fresh)
             #bottles
             #kdePackages.sddm-kcm
             kdePackages.kweather
