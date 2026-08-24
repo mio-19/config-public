54e9f724f2b992fac6427b098805b816c74df0fa
Mio <mio@mio19.uk>
2026-08-25 00:00:16 +1000

    save

--- modules/games.nix
+++ modules/games.nix
@@ -68,7 +68,6 @@
 
         # https://github.com/librephoenix/nixos-config/blob/0c3b676ab9d3e93780f06dbe5e084048eeed9a32/modules/system/security/firejail/default.nix#L24
         programs.firejail.wrappedBinaries = {
-          # cannot launch from steam if sandboxed
           prismlauncher = {
             executable = "${cleanPkg (offloadPkg pkgs.prismlauncher)}/bin/prismlauncher";
             # https://github.com/librephoenix/nixos-config/raw/0c3b676ab9d3e93780f06dbe5e084048eeed9a32/modules/system/security/firejail/profiles/prismlauncher.profile
