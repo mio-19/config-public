9c7d872421102161829a09d8bf82a5461cd670cf
Mio <mio@mio19.uk>
2026-09-05 22:23:35 +1000

    enable-flyline = !stdenv.hostPlatform.isDarwin && pkgs ? flyline; # flyline feels wrong on darwin

--- modules/users/_/cli.nix
+++ modules/users/_/cli.nix
@@ -18,7 +18,7 @@ let
   enable-zsh-patina = pkgs ? nur && pkgs.nur.repos ? mio && pkgs.nur.repos.mio ? zsh-patina; # inputs.mio.packages.${pkgs.stdenv.hostPlatform.system} ? zsh-patina || pkgs ? zsh-patina;
   zsh-patina = pkgs.nur.repos.mio.zsh-patina; # inputs.mio.packages.${pkgs.stdenv.hostPlatform.system}.zsh-patina or pkgs.zsh-patina;
   enable-zsh-sage = false;
-  enable-flyline = pkgs ? flyline;
+  enable-flyline = !stdenv.hostPlatform.isDarwin && pkgs ? flyline; # flyline feels wrong on darwin
   inherit (pkgs) stdenv;
   bash3workaround = stdenv.hostPlatform.isDarwin;
 in
