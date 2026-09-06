81fbf393d2d1c7c1bde50d63c9aa6a778a44d526
Mio <mio@mio19.uk>
2026-09-06 20:14:17 +1000

    inherit (pkgs-pin3) vscode vscode-fhs vscode-extensions;

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -44,6 +44,7 @@
               inherit (pkgs') freecad; # no binary cache with cuda and no binary cache with rocm
               inherit (pkgs') firefox-esr firefox-esr-unwrapped;
               inherit (pkgs-pin2) jabref;
+              inherit (pkgs-pin3) vscode vscode-fhs vscode-extensions;
             }
           )
           inputs.chaotic.overlays.default
