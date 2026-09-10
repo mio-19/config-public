8ad24fadc40078976984ac00babbfbfddecd6ed0
Mio <mio@mio19.uk>
2026-09-10 20:10:15 +1000

    Revert "sublimetext4 fine?"
    This reverts commit 0dbb02e90fe97069b134c5866c4f64076cc11dbb.

--- modules/common.nix
+++ modules/common.nix
@@ -62,6 +62,9 @@
             ];
         };
 
+        nixpkgs.config.problems.handlers = {
+          "sublimetext4".removal = "ignore";
+        };
         nixpkgs.config.permittedInsecurePackages =
           with pkgs;
           map (pkg: pkg.name) [
