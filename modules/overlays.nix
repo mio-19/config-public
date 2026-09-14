15cac6c53b739503f8ac03103a8726c40f59dc10
Mio <mio@mio19.uk>
2026-09-14 17:03:38 +1000

    inherit (pkgs-pin5) lean4;

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -85,6 +85,7 @@
               raycast = mio.raycast_macos15;
               #harmonia = mio.harmonia_patched;
               inherit (pkgs-pin7) jadx;
+              inherit (pkgs-pin5) lean4;
             }
           )
         ];
