82c78c8e19abe845c5f4487e4173ae058ec1c836
Mio <mio@mio19.uk>
2026-09-22 18:02:29 +1000

    fix(fw13): enable systemd-resolved for secure wireguird dns

--- modules/hosts/fw13/_nixos/default.nix
+++ modules/hosts/fw13/_nixos/default.nix
@@ -42,6 +42,7 @@ with _include;
   security.allowSimultaneousMultithreading = true;
 
   programs.wireguird.enable = true;
+  services.resolved.enable = true;
 
   persistent_power-profiles-daemon = false;
 
