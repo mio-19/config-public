e43bf759562f6b06882967735a2eb74983e65bd2
Mio <mio@mio19.uk>
2026-09-23 12:07:41 +1000

    less pin mac

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -95,7 +95,6 @@
               starship = mio.starship_patched;
               raycast = mio.raycast_macos15;
               #harmonia = mio.harmonia_patched;
-              inherit (pkgs-pin5) lean4;
             }
           )
         ];
