4087a152aeb0260b0f684e5ba73b57586c734c92
Mio <mio@mio19.uk>
2026-09-25 23:27:01 +1000

    #

--- modules/desktop-offline.nix
+++ modules/desktop-offline.nix
@@ -131,7 +131,7 @@
         systemPackages_hardened =
           with pkgs;
           [
-            inputs.mio.packages.${pkgs.stdenv.hostPlatform.system}.bambu-studio-open
+            #inputs.mio.packages.${pkgs.stdenv.hostPlatform.system}.bambu-studio-open
             orca-slicer
             inputs.mio.packages.${pkgs.stdenv.hostPlatform.system}.jetbrains_idea-oss # jetbrains.idea-oss
             openshot-qt
